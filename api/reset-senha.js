4f09cf18da913fbf2541d1adecf438a39687778c
same1ahvn <same1ahvn@gmail.com>
2026-03-30 09:09:40 -0300

    Update reset-senha.js

--- api/reset-senha.js
+++ api/reset-senha.js
@@ -12,10 +12,11 @@ export default async function handler(req, res) {
     const hash = await bcrypt.hash("123456", 10);
 
     await pool.query(
-      "UPDATE usuarios SET senha = $1 WHERE username = 'YURI'"
-    , [hash]);
+      "UPDATE usuarios SET senha = $1",
+      [hash]
+    );
 
-    res.json({ sucesso: true, senha: "123456" });
+    res.json({ sucesso: true, senha: "123456 PARA TODOS" });
 
   } catch (err) {
     res.status(500).json({ erro: err.message });
