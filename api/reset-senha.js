142f8c60af8ce31625151512503e4a4d70b4e26b
same1ahvn <same1ahvn@gmail.com>
2026-03-30 09:05:32 -0300

    Create reset-senha.js

---
+++ api/reset-senha.js
@@ -0,0 +1,23 @@
+import pkg from 'pg';
+import bcrypt from 'bcryptjs';
+
+const { Pool } = pkg;
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+});
+
+export default async function handler(req, res) {
+  try {
+    const hash = await bcrypt.hash("123456", 10);
+
+    await pool.query(
+      "UPDATE usuarios SET senha = $1 WHERE username = 'admin'"
+    , [hash]);
+
+    res.json({ sucesso: true, senha: "123456" });
+
+  } catch (err) {
+    res.status(500).json({ erro: err.message });
+  }
+}
